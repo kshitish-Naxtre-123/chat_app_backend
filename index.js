eb02bf33a01b5205d5ff4fbf88c2f043d5fdfdfb
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-06-10 17:57:48 +0530

    updated with origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:process.env.FRONTEND_URL,
+    origin:"http://localhost:3000",
     credentials:true
   })
 );
--- socket/index.js
+++ socket/index.js
@@ -15,7 +15,7 @@ dotenv.config({ path: ".env" });
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin:"http://localhost:3000",
     credentials: true,
   },
 });
