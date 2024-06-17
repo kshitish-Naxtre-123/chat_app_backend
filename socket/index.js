0d5a35ba1ef603dcf442a9babde6399829ac1a31
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-06-17 16:38:08 +0530

    updated

--- socket/index.js
+++ socket/index.js
@@ -28,9 +28,9 @@ io.on("connection", async (socket) => {
 
   //current user details
   const user = await getUserDetailsFromToken(token);
-
+  
   //create a room
-  socket.join(user?._id.toString());
+  socket.join(user?._id);
   onlineUser.add(user?._id.toString());
 
   io.emit("onlineUser", Array.from(onlineUser));
